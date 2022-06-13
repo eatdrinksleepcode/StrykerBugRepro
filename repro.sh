0dfbb208aaf189a23d2bd64031722276cd53d47c
David Nelson <dnelson@gitlab.com>
2022-06-13 00:54:12 -0500

    Update repro steps

--- repro.sh
+++ repro.sh
@@ -4,5 +4,5 @@ find . -name "*AssemblyInfo.cs" # There are no AssemblyInfo.cs files before buil
 dotnet build
 find . -name "*AssemblyInfo.cs" # Each project has a generated AssemblyInfo.cs file at the configured IntermediateOutputPath
 dotnet clean -v minimal && rm -r **/bin **/obj
-dotnet stryker -tp StrykerBugReproTests -p StrykerBugRepro
+dotnet stryker --log-to-file -tp TestProj -p MainProj
 find . -name "*AssemblyInfo.cs" # In addition to the AssemblyInfo.cs files at the configured IntermediateOutputPath, there is a duplicate AssemblyInfo.cs file at the same relative path under the project directory
